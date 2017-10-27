236e753fdcc9830d46ce271f043f22168452e225
Rafael de Oliveira Benetti <rafaeldeoliveirabenetti@gmail.com>
2017-10-27 14:26:46 -0200

    example(config): add more feats on webpack configuration.

--- 00 - basics/00 - config/01 - webpack 02 advanced/webpack.config.js
+++ 00 - basics/00 - config/01 - webpack 02 advanced/webpack.config.js
@@ -3,24 +3,24 @@
 
     const HtmlWebpackPlugin = require('html-webpack-plugin');
     const ExtractTextPlugin = require("extract-text-webpack-plugin");
+    const webpack = require('webpack');
 
     const root = `${__dirname}/src/`;
     const dist = `${root}/dist`;
 
     module.exports = {
-        entry: `${root}/js/app.js`,
+        entry: {
+            app: `${root}/js/app.js`,
+            contact: `${root}/js/contact.js`
+        },
         output: {
             path: dist,
-            filename: 'script.min.js'
+            filename: '[name].min.js'
         },
         module: {
             rules: [{
                     test: /\.scss$/,
-                    use: ExtractTextPlugin.extract({
-                        fallback: 'style-loader',
-                        use: ['css-loader', 'sass-loader'],
-                        publicPath: dist
-                    })
+                    use: ['style-loader', 'css-loader', 'sass-loader']
                 },
                 {
                     test: /\.js$/,
@@ -31,20 +31,30 @@
         },
         devServer: {
             contentBase: dist,
-            compress: true,
             stats: 'errors-only',
-            open: true
+            compress: true,
+            open: true,
+            hot: true
         },
         plugins: [
             new HtmlWebpackPlugin({
                 template: `${root}/index.html`,
+                excludeChunks: ['contact'],
+                hash: true
+            }),
+            new HtmlWebpackPlugin({
+                template: `${root}/contact.html`,
+                filename: 'contact.html',
+                excludeChunks: ['app'],
                 hash: true
             }),
             new ExtractTextPlugin({
                 filename: 'style.min.css',
-                disable: false,
+                disable: true,
                 allChunks: true
-            })
+            }),
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.NamedModulesPlugin()
         ]
     }
 })();
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # try-react
-Project to learn how react works.
+ Project to learn how to configure and work with react apps. In this project I will keep all the examples and exercises during my learning path.
+
+## Basics
+ - [Webpack 02](https://github.com/rafaelbenetti/try-react/tree/master/00%20-%20basics/00%20-%20config/00%20-%20webpack%2002)
+    + Bundle js
+    + Babel (es2015, react, presets-0)
+    + Sass (concat, minify, transpile)
+    + Seppareted css file
+    + Html template
+    + Webpack-dev-server
+ - [Webpack 02 advanced](https://github.com/rafaelbenetti/try-react/tree/master/00%20-%20basics/00%20-%20config/01%20-%20webpack%2002%20advanced)
+    + Multi Html templates 
+    + delete files to prod (rimraf)
+    + Hot module replacement 
