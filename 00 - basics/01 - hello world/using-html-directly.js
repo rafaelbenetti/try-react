b86e26ca404733bbc63be184d0b0ab1858301d41
Rafael de Oliveira Benetti <rafaeldeoliveirabenetti@gmail.com>
2017-10-28 10:59:20 -0200

    feat(examples): add more react examples.

---
+++ 00 - basics/01 - hello world/using-html-directly.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div>
+        <h2>Hello react!</h2>
+        <p>This is how to render html directly!</p>
+    </div>,
+    document.getElementById('root')
+);
---
+++ 00 - basics/04 - import and export/App.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h2>First component</h2>
+            <p>This is how you export/import a new component.</p>
+        </div>
+    );
+}
+
+export default App;
